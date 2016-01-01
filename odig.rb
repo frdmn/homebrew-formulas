4fdd11df28cb7479eb5a8c8f5b98f7657083ab05
Jonas Friedmann <j@frd.mn>
2016-01-01 14:20:05 +0100

    Release odig version 1.0.1

--- odig.rb
+++ odig.rb
@@ -1,9 +1,9 @@
 class Odig < Formula
   desc "Bash script to lookup hostname and PTR record of a given IP address at a stroke"
   homepage "https://github.com/frdmn/odig"
-  url "https://github.com/frdmn/odig/archive/1.0.0.tar.gz"
-  version "1.0.0"
-  sha256 "2134e9505802ebc167beef2a8a15b9b6060de99da6689bf917bca774842ccce0"
+  url "https://github.com/frdmn/odig/archive/1.0.1.tar.gz"
+  version "1.0.1"
+  sha256 "6b73aa33cd76276de9ca3e0b36ceefe5177cb256f522f2ec1df4f219db8f0c75"
 
   def install
     ENV.deparallelize
