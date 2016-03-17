88b8130a142f667450a4bfad0902924c50ffa5ed
Jonas Friedmann <j@frd.mn>
2016-03-17 12:08:35 +0100

    Release uping 0.2.2

--- uping.rb
+++ uping.rb
@@ -1,9 +1,9 @@
 class Uping < Formula
   desc "Ping tool that stops as soon as the host comes back online"
   homepage "https://github.com/frdmn/uping"
-  url "https://github.com/frdmn/uping/archive/0.2.1.tar.gz"
-  version "0.2.1"
-  sha256 "0dfc21fa98ec5b8970f1c7a487776485f5569a30a3e96f927aac165de487e2b2"
+  url "https://github.com/frdmn/uping/archive/0.2.2.tar.gz"
+  version "0.2.2"
+  sha256 "776bbd1fceceee36ea3611d8258e53edfbe71751ae5690a523fdf9b9fdef7338"
 
   def install
     ENV.deparallelize
