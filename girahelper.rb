a4a1781131e6c157d41f0bbcc78a3d06cd6a2a0f
Jonas Friedmann <j@frd.mn>
2017-01-02 15:55:50 +0100

    Update Girahelper

--- girahelper.rb
+++ girahelper.rb
@@ -1,15 +1,15 @@
 class Girahelper < Formula
   desc "Bash script to control a Gira home server"
   homepage "https://github.com/frdmn/GiraHelper"
-  url "https://github.com/frdmn/GiraHelper/archive/1.0.0.tar.gz"
-  version "1.0.0"
-  sha256 "5e55e292dda51328ed98a285413346441fe2695396d9b2a1db5455a69a1b9bbf"
+  url "https://github.com/frdmn/GiraHelper/archive/1.0.2.tar.gz"
+  version "1.0.2"
+  sha256 "fdef756b2c7eab0f19620210b2cfd1b1494016c0afa687b5db5de5ab044d39c8"
 
   depends_on "terminal-notifier"
 
   def install
     ENV.deparallelize
-    prefix.install %w{gira.conf lib}
+    prefix.install %w{gira.json lib}
     bin.install "gira"
   end
 
