ac2bd5cd95c04211095065b333dddbdc6b155bef
Jonas Friedmann <j@frd.mn>
2017-05-21 17:33:00 +0200

    Use master for bork-dev

--- bork-dev.rb
+++ bork-dev.rb
@@ -1,7 +1,7 @@
 class BorkDev < Formula
   desc "Bash DSL for config management"
   homepage "https://github.com/frdmn/bork"
-  head "https://github.com/frdmn/bork.git", :branch => "bug/homebrew-1.0.0-compatibility"
+  head "https://github.com/frdmn/bork.git"
 
   bottle :unneeded
 
