314a19874af8574c357c20b05203dbcce8755bbd
Jonas Friedmann <j@frd.mn>
2018-08-13 17:30:26 +0200

    Bump dev-search version to 1.2.3

--- dev-search.rb
+++ dev-search.rb
@@ -1,9 +1,9 @@
 class DevSearch < Formula
   desc "Dead simple project search tool written in Bash with fuzzy support"
   homepage "https://github.com/frdmn/dev-search"
-  url "https://github.com/frdmn/dev-search/archive/1.2.2.tar.gz"
-  version "1.2.2"
-  sha256 "5f7b28f3ba4fc9a6db854b9549e65d0bae93e9c0ef059fc91b157ac69b238b26"
+  url "https://github.com/frdmn/dev-search/archive/1.2.3.tar.gz"
+  version "1.2.3"
+  sha256 "482767757ede7e8695ff808d7f5ecda08cb4704bc10a18c1aca52c4f76fcd010"
 
   def install
     ENV.deparallelize
